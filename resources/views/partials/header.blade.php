9ad274f1bc1c5382a582494a081c2be565aaa811
JoseManuel-Feliz <jmgarcia2120@gmail.com>
2024-10-23 09:47:56 +0200

    added foreach loop to iterate header data in header.php

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,1 +1,34 @@
-{{"header goes here"}}
+<header>
+
+    <div class="bg-blu">
+        <span>
+            DC POPWER <small>logo font awesome</small>
+            VISA <small>logo font awesome</small>
+        </span>
+
+        <span>
+            ADDITIONAL SITES <small>logo font awesome</small>
+        </span>
+    </div>
+    <div class="container">
+
+        <nav class="navbar wrapper">
+            <div class="logo-wrapper">
+                <img src="{{URL::asset('images/dc-logo.png')}}" alt="">
+            </div>
+
+            <ul class="gap-20">
+                @foreach($links as $link)
+                <li>
+                    <a href="#"> {{$link}} </a>
+                </li>
+                @endforeach
+            </ul>
+
+            <div>
+                <input type="text" placeholder="Search">
+            </div>
+        </nav>
+
+    </div>
+</header>
