f36a40b5bf1f6c7339eeafb27c259d643c2accf1
JoseManuel-Feliz <jmgarcia1718@gmail.com>
2024-10-23 09:57:34 +0200

    added foreach loop to iterate comics data in comics.php

--- resources/views/pages/comics.blade.php
+++ resources/views/pages/comics.blade.php
@@ -2,7 +2,43 @@
 
 @section("main-content")
 
-<h1>
-    {{"prova"}}
-</h1>
+<section>
+    <div>
+        <!-- <img src="{{URL::asset('images/jumbotron.jpg')}}" alt=""> -->
+    </div>
+</section>
+<section>
+    <div class="content-wrapper container">
+        <ul>
+            @foreach($comics as $comic)
+            <li>
+                <a href="#">
+                    <h3>{{$comic['title']}}</h3>
+                    <img src="{{$comic['thumb']}}" alt="$comic['title']">
+                    <p>{{$comic['description']}}</p>
+                    <p>{{$comic['price']}}</p>
+                    <p>{{$comic['series']}}</p>
+                    <p>{{$comic['sale_date']}}</p>
+                    <p>{{$comic['type']}}</p>
+                </a>
+            </li>
+            @endforeach
+        </ul>
+    </div>
+    <div class="btn-wrapper">
+        <a href="#" class="btn">Load more</a>
+    </div>
+</section>
+
+<section class="main-navbar">
+    <nav class="container">
+        <ul>
+            <!--   @//foreach() -->
+            <li>
+            </li>
+            <!--   @//endforeach -->
+        </ul>
+    </nav>
+
+</section>
 @endsection
