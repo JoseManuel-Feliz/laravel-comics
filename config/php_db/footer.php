dda538a8d2ec244c5fe536e152be8124e3b0c0d2
JoseManuel-Feliz <jmgarcia1718@gmail.com>
2024-10-23 09:52:42 +0200

    added fotter,header,comics data

---
+++ config/php_db/footer.php
@@ -0,0 +1,50 @@
+<?
+return [
+    "footer_links" =>
+    [
+        "dc comics" =>
+        [
+            "characters",
+            "comics",
+            "movies",
+            "TV",
+            "games",
+            "videos",
+            "news",
+        ],
+        "shop" =>
+        [
+            "shop dc",
+            "shop dc collectibles",
+        ],
+        "dc" =>
+        [
+            "terms of use",
+            "privacy policy (new)",
+            "ad choices",
+            "advertising",
+            "jobs",
+            "subscriptions",
+            "talent workshops",
+            "cpsc certificates",
+            "ratings",
+            "shop help",
+            "contact us"
+        ],
+        "sites" => [
+            "dc",
+            "mad magazine",
+            "dc kids",
+            "dc universe",
+            "dc power visa",
+        ]
+    ],
+    "img_paths" =>
+    [
+        "facebook.png",
+        "periscope.png",
+        "pinterest.png",
+        "twitter.png",
+        "youtube.png"
+    ]
+];
