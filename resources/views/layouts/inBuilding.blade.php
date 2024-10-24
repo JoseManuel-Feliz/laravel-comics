b838169dd4c035753e490f2d944e71802dbaf6a0
JoseManuel-Feliz <jmgarcia2120@gmail.com>
2024-10-24 09:47:05 +0200

    added imgs errors and inbuilding layout

---
+++ resources/views/layouts/inBuilding.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+@yield("cdn","")
+
+<title>@yield("page-title","")</title>
+
+<body>
+    @include("partials.header")
+    <main>
+        @yield("main-content")
+    </main>
+
+    @vite("resources/js/app.js")
+    @include("partials.footer")
+</body>
+
+</html>
